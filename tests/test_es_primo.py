a9252f31ea5569a01c21b02391c616e27812327e
k1ber8 <keilyheredia2707@gmail.com>
2026-03-02 20:19:51 -0700

    Fix path for GitHub Actions

--- tests/test_es_primo.py
+++ tests/test_es_primo.py
@@ -1,20 +1,26 @@
-import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from es_primo import es_primo
 
-
 def test_numero_primo():
+    """Caso correcto"""
     assert es_primo(7) is True
 
 
 def test_numero_no_primo():
+    """Caso límite - número válido pero no primo"""
     assert es_primo(8) is False
 
 
 def test_error_numero_menor_que_dos():
+    """Caso error"""
     with pytest.raises(ValueError):
         es_primo(1)
 
 
 def test_error_tipo_incorrecto():
+    """Caso error tipo"""
     with pytest.raises(TypeError):
         es_primo("10")
